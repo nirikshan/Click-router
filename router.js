da9a02ce73003f134a0d075aa6bec1a06e23caa4
nirikshan <nirikshanbhusal@gmail.com>
2019-04-13 16:43:31 +0545

    1 level Update
    1)  Code reduction .
    2)  added 'SRC'.
    3)  2 level binding between url and view with "CurrentUrl"

--- router.js
+++ router.js
@@ -21,15 +21,89 @@ ex = {
     routes:[],
     root:GetRoot(),
     play:null,
+    CurrentUrl:CreateNow(GetMode('history')),
     now:{
       origin:location.origin,
       parm:CreateNow('history'),
     },
 },
+syncComponent = function(callack){
+    var match , got    =  ex.CurrentUrl.split('/').filter((item) => item != '' && item !== '#');
+    for (let j = 0; j < ex.routes.length; j++) {
+        var el     =  ex.routes[j],
+            path   =  el._path,
+            syntax =  path.split('/').filter((item) => item != ''),
+            match  =  {
+                index:0,
+                match:false
+            },
+            parmeter = {};        
+            if(got.length > syntax.length || got.length < syntax.length){
+                if(ex.CurrentUrl == '/' || ex.CurrentUrl == ''){
+                    return callack(ex.routes[0] , parmeter);
+                }
+            }else{
+                for (var i = 0; i < got.length; i++) {
+                    var x  =  got[i],
+                        y  =  syntax[i];
+                    if(y[0] !== ':'){//no problem avoiding changing particals  
+                        if(x == y){
+                            match.index = j;
+                            match.match = true;
+                        }else{
+                            match.index = 0;
+                            match.match = false;
+                        }
+                    }else{
+                        parmeter[y.substr(1)] = x;    
+                    }
+                }
+            }
+            if(match.match){
+                return callack(ex.routes[match.index] , parmeter);
+            }
+    }
+    callack(ex.routes['*'] , parmeter)
+},
+WatchComponent = function(ax){
+  syncComponent(function (a , b) {
+     if(a.src && typeof a.src === 'function'){
+        a.src().then(o=>{
+            ax(a , b); 
+            ex.play = '#1'
+        })
+      }else{
+        ax(a , b);
+          if(ex.watch == undefined){
+            setTimeout(() => { 
+                ex.play = '#2'
+            }, 30);
+          }else{
+            ex.play = '#2'
+          }
+          
+      }
+  });
+},
+locate = function(route){
+    if(ex.mode === 'history'){
+        window.history.pushState(null , null , ex.root + route);
+    }else{
+        route = route.replace(/^\//,'');
+        window.location.href = window.location.href.replace(/#(.*)$/,'') + '#/' + route;
+    } 
+    
+},
+UrlChange = function() {
+    WatchComponent(function(a , b) {
+        ex.now.target = ((ex.CurrentUrl == '/' || ex.CurrentUrl == '') ? ex.routes[0] : a);
+        ex.now.parmeter = b;
+    })
+},
 handelChange = function($el , Manage , id , node , ComponentName , ComponentId){
-    console.log('init')
     $el.innerHTML = null;
     Manage($el , ex.now.target.component , id , node.props , ComponentName , ComponentId)
+    typeof ex.now.target.handler == 'function' && ex.now.target.handler()
 },
 observeObject = function(data, key, val) {
     Object.defineProperty(data, key, {
@@ -44,6 +118,9 @@ observeObject = function(data, key, val) {
                 val = GetMode(newVal);
             }else if(key == 'play'){
                 typeof ex.watch == 'function' && ex.watch(handelChange);
+            }else if(key == 'CurrentUrl'){
+                val = newVal;
+                UrlChange(val);
             }else{
                 val = newVal;
             }
@@ -55,111 +132,22 @@ init = function init(a) {
         observeObject(ex, key, a[key]);
     }
 },
-go = function(route){
-    match((route ? route : ''));
-},
-match = function(route){
-    for (let i = 0; i < ex.routes.length; i++) {
-        let paramNames = [];
-        let regexPath = ex.routes[i].path.replace(/([:*])(\w+)/g , function(full , colon , component) {
-            paramNames.push(component);
-            return('([^\/]+)');
-        }) + '(?:\/|$)';
-        let routrMatch = route.match(new RegExp(regexPath));
-        if(routrMatch !== null){
-             var parms = routrMatch.slice( 1 , routrMatch.length).reduce((parms , value , index)=>{
-                if(parms === null) parms = {};
-                parms[paramNames[index]] = value;
-                return parms;
-             } , null);
-             if(parms == null){
-                ex.routes[i].handler();
-             }else{
-                ex.routes[i].handler(parms);
-             }
-             locate(route)
-        }
+go = function(path) {
+    ex.CurrentUrl = path;
+    if(ex.now.target._path !== '404'){
+        locate(path)
     }
-},
-syncComponent = function(callack){
-    var match , got    =  ex.now.parm.split('/').filter((item) => item != '' && item !== '#');
-    for (let j = 0; j < ex.routes.length; j++) {
-        var el     =  ex.routes[j],
-            path   =  el._path,
-            syntax =  path.split('/').filter((item) => item != ''),
-            match  =  {
-                index:0,
-                match:false
-            };        
-            if(got.length > syntax.length || got.length < syntax.length){
-            }else{
-                for (var i = 0; i < got.length; i++) {
-                    var x  =  got[i],
-                        y  =  syntax[i];
-                    if(y[0] !== ':'){//no problem avoiding changing particals  
-                        if(x == y){
-                            match.index = j;
-                            match.match = true;
-                        }else{
-                            match.index = 0;
-                            match.match = false;
-                        }
-                    }
-                }
-            }
-            if(match.match){
-                ex.now.target = (ex.routes[match.index])
-                return callack(ex.now.target);
-            }
-    }
-    ex.now.target = ex.routes['*'];
-    callack(ex.now.target)
-},
-WatchComponent = function(ax){
-  syncComponent(function (a) {
-      if(a.src && typeof a.src === 'function'){
-        a.src()
-        ax();
-      }else{
-        ax();
-      }
-      setTimeout(function(params) {
-        ex.play = '#1'
-      },50)
-  });
-},
-Check = function Check() {
-    var def = ex.routes[0],
-        now = CreateNow(ex.mode);
-
-    WatchComponent(function() {
-       if(now == '/' || now == ''){
-         go(def._path)
-       } 
-    });
-},
-locate = function(route){
-    ex.now.parm = route;
-    WatchComponent(function() {
-        if(ex.mode === 'history'){
-            window.history.pushState(null , null , ex.root + route);
-        }else{
-            route = route.replace(/^\//,'');
-            window.location.href = window.location.href.replace(/#(.*)$/,'') + '#/' + route;
-        } 
-    });
 },
 Router = function Router(a , b) {
+        init(ex);
         ex.name = a;
         ex.mode = GetMode(b.mode);
         b.route.map(el => { this.add(el)});
         ex.root = GetRoot(b.root);
         ex.now  = {
-             origin:location.origin,
-             parm:CreateNow(ex.mode),
-        };  
-        init(ex);
-        Check();
+             origin:location.origin
+        };
+        UrlChange();
         return ex;
 };
 Router.prototype.add = function(route){
@@ -169,9 +157,7 @@ Router.prototype.add = function(route){
     ex.routes.push(new Route(route.component , route.path , route.handle , route.src));
 }
 window.addEventListener('popstate', function (e) {
-    ex.now.parm = CreateNow(ex.mode);
-    Check()
-    ex.play = 'Nirikshan Bhusal';
+    ex.CurrentUrl = CreateNow(ex.mode);
 }); 
 export {
     Router,
